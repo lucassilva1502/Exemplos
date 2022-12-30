ace37099ac181962542e731f0efde18564c7506e
lucassilva1502 <121691755+lucassilva1502@users.noreply.github.com>
2022-12-30 16:58:55 -0300

    Add files via upload

---
+++ ex03.py
@@ -0,0 +1,10 @@
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite mais um número: '))
+a = n1 + n2
+s = n1 - n2
+m = n1 * n2
+d = n1 / n2
+di = n1//n2
+e = n1 ** n2
+print('Essa adição vale {}, subtração vale {}, multiplicação vale {}, divisão vale {}'.format(a,s,m,d))
+print('Divisão inteira vale {}, Potencia vale {}'.format(di,e))
